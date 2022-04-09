1b9fdb26b1c29f2848104725f0630d13518bb94d
Matthijn Dijkstra <matthijndijkstra@gmail.com>
2022-04-09 10:19:36 +0200

    Simple email protection + improved texts

--- main.js
+++ main.js
@@ -9,10 +9,21 @@ function debounce(func, timeout = 300){
 window.addEventListener('load', (event) => {
     initPhysics()
     setupAnimation()
+    unhideEmail()
 });
 
 if(!/Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
     window.onresize = debounce(function () {
         initPhysics()
+    })
+}
+
+// Just to thwart the basic email collectors
+function unhideEmail() {
+    const emailLink = document.querySelector("[data-mail]")
+    emailLink.addEventListener('mouseenter', () => {
+        const data = emailLink.getAttribute("data-mail")
+        const decoded = atob(data)
+        emailLink.href = `mailto: ${decoded}`
     })
 }
